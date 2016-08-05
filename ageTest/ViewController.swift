710a8c73ea1c492ca21064820e7fbb954dc0fe67
YourboyE <YourBoyE@Edward-Garcias-MacBook-Pro.local>
2016-08-04 19:42:41 -0700

    commiting all the works inside the Desktop AgeTest Folder

--- ageTest/ViewController.swift
+++ ageTest/ViewController.swift
@@ -10,16 +10,76 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //variables
+    var maxAge = 0
+    
+    // Outlets
+    @IBOutlet weak var whatAgeLogo: UIImageView!
+    @IBOutlet weak var enterAgeTxt: UITextField!
+    @IBOutlet weak var playBtn: UIButton!
+    
+    @IBOutlet weak var playAgain: UIButton!
+    @IBOutlet weak var ageMessage: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //Looks for single or multiple taps.
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
+    }
+    
+    //Calls this function when the tap is recognized.
+    func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    @IBAction func onPlayBtnPressed(sender:UIButton!){
+        
+        if enterAgeTxt.text != nil && enterAgeTxt.text != "" {
+        
+        whatAgeLogo.hidden = true
+        enterAgeTxt.hidden = true
+        playBtn.hidden = true
+        
+        playAgain.hidden = false
+        ageMessage.hidden = false
+            
+        maxAge = Int(enterAgeTxt.text!)!
+            
+        updateLabel()
     }
+    }
+    
+    func updateLabel(){
+        
+        if (maxAge <= 21){
+            ageMessage.text = "Your Still Young!"
+        } else if (maxAge >= 22 && maxAge <= 70) {
+            ageMessage.text = "Your Getting Older!"
+        } else if (maxAge >= 71){
+            ageMessage.text = "Wow Your Old!"
+            
+        }
 
-
+    }
+    
+    
+    @IBAction func onPlayAgainBtnPressed(sender: UIButton) {
+        
+        whatAgeLogo.hidden = false
+        enterAgeTxt.hidden = false
+        playBtn.hidden = false
+        
+        playAgain.hidden = true
+        ageMessage.hidden = true
+        
+        enterAgeTxt.text = ""
+        
+    }
 }
+
 
