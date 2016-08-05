5aa0bf7207b5c7a18718b20792b1970585d59425
Edward Garcia <edwardgarcia@Macintosh.local>
2016-08-04 20:18:42 -0700

    adding all files

--- AgeGame/ViewController.m
+++ AgeGame/ViewController.m
@@ -10,9 +10,38 @@
 
 @interface ViewController ()
 
+//variables
+
+
+//outlets
+@property (weak, nonatomic) IBOutlet UIImageView *WhatsAgeLogo;
+
+@property (weak, nonatomic) IBOutlet UITextField *EnterAgeTxt;
+
+@property (weak, nonatomic) IBOutlet UIButton *PlayBtn;
+
+
+@property (weak, nonatomic) IBOutlet UIButton *PlayAgnBtn;
+@property (weak, nonatomic) IBOutlet UILabel *AgeTextField;
+
+
+
+
+
+
+
 @end
 
 @implementation ViewController
+
+
+- (IBAction)PlayBtnPressed:(UIButton *)sender {
+    _WhatsAgeLogo.hidden = true;
+    _EnterAgeTxt.hidden = true;
+    _PlayBtn.hidden = true;
+    
+    _AgeTextField.hidden = false;
+}
 
 - (void)viewDidLoad
 {
@@ -25,5 +54,7 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
 
 @end
